de7cab91cbac421c9227c8a015d978dfdfb108a9
hlibine <hugo.libine@gmail.com>
2024-02-22 17:26:39 +0100

    starting out

---
+++ srcs/so_long.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlibine <hlibine@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/22 11:19:52 by hlibine           #+#    #+#             */
+/*   Updated: 2024/02/22 14:10:24 by hlibine          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <math.h>
+# include "../libs/extended_ft/srcs/extended_ft.h"
+
+# ifdef __APPLE__
+#  include "../libs/minilibx_opengl/mlx.h"
+# endif
+
+# ifndef __APPLE__
+#  include "../libs/minilibx-linux/mlx.h"
+# endif
+
+#endif
