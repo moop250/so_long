6ce586b20c3329083fba912eb35fa36dd99bb597
hlibine <hugo.libine@gmail.com>
2024-03-04 14:32:09 +0100

    map checking works

--- srcs/pathing.c
+++ srcs/pathing.c
@@ -6,7 +6,7 @@
 /*   By: hlibine <hlibine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 15:58:59 by hlibine           #+#    #+#             */
-/*   Updated: 2024/03/04 12:48:31 by hlibine          ###   ########.fr       */
+/*   Updated: 2024/03/04 14:29:42 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	dfs(t_map *map, char **tmp_map, t_pos search, t_pos out)
 {
 	if (search.x < 0 || search.y < 0 ||
 			search.x > map->width || search.y > map->length
-			|| tmp_map[search.x][search.y] == 1)
+			|| tmp_map[search.y][search.x] == '1')
 		return (-1);
 	if ((search.x == out.x && search.y == out.y)
 			|| (search.x == out.x && search.y + 1 == out.y)
@@ -24,7 +24,7 @@ int	dfs(t_map *map, char **tmp_map, t_pos search, t_pos out)
 			|| (search.x + 1 == out.x && search.y == out.y)
 			|| (search.x - 1 == out.x && search.y == out.y))
 		return (5);
-	tmp_map[search.x][search.y] = 1;
+	tmp_map[search.y][search.x] = '1';
 	search.x++;
 	if (dfs(map, tmp_map, search, out) >= 0)
 		return (RIGHT);
@@ -57,13 +57,13 @@ void	set_ents(t_map *map)
 		{
 			if (map->data[i[0]][i[1]] == 'P')
 			{
-				spawn->x = i[0];
-				spawn->y = i[1];
+				spawn->x = i[1];
+				spawn->y = i[0];
 			}
 			else if (map->data[i[0]][i[1]] == 'E')
 			{
-				exit->x = i[0];
-				exit->y = i[1];
+				exit->x = i[1];
+				exit->y = i[0];
 			}
 		}
 	}
@@ -80,10 +80,10 @@ char	**mapdup(char **data)
 
 	i = 0;
 	a = -1;
-	len = ft_strlen(data[0]);
+	len = ft_strlen(data[0]) + 1;
 	while (data[i])
 		++i;
-	map = galloc(i * sizeof(char *));
+	map = galloc((i + 1) * sizeof(char *));
 	while (++a < i)
 	{
 		map[a] = galloc(len * sizeof(char));
@@ -99,9 +99,9 @@ void	check_path(t_map *map)
 	t_pos	spawn;
 	t_pos	out;
 
+	set_ents(map);
 	spawn = *map->spawn;
 	out = *map->exit;
-	set_ents(map);
 	map2 = mapdup(map->data);
 	if (dfs(map, map2, spawn, out) < 0)
 		sl_error("pathing error: no path found");
